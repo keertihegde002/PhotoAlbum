effae717805af631acbecef607a1159642d9fd3d
keerti <keertihegde002@gmail.com>
2023-08-01 11:54:17 +0530

    rendered and redirected

--- photos/views.py
+++ photos/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Category,Photo
 # Create your views here.
 def gallery(request):
+    category=request.GET.get('category')
+
+    if category is None:
+        photos=Photo.objects.all()
+    else:
+        photos=Photo.objects.filter(category__name__contains=category)
+
+
+
     categories=Category.objects.all()
-    photos=Photo.objects.all()
+    
     context={'categories': categories,'photos': photos} 
     return render(request, 'photos/gallery.html',context)
 
@@ -13,4 +22,25 @@ def viewPhoto(request,pk):
     return render(request, 'photos/photo.html',context)
 
 def addPhoto(request):
-    return render(request, 'photos/add.html')
+    categories=Category.objects.all()
+
+    if request.method == 'POST':
+        data=request.POST
+        image=request.FILES.get('image')
+
+        if data['category']!='none':
+            category=Category.objects.get(id=data['category'])
+        elif data['category_new']!='':
+            category,created=Category.objects.get_or_create(name=data['category_new'])
+        else:
+            category=None
+
+        photo=Photo.objects.create(
+            category=category,
+            description=data['description'],
+            image=image,
+        )
+        return redirect('gallery')
+    context={'categories': categories}
+    return render(request, 'photos/add.html',context)
+
